d51d2059e551c7146d82e05bfe25c79c0c54e9d0
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-03-06 10:35:51 -0300

    up

---
+++ exercicios/ex022/main.js
@@ -0,0 +1,9 @@
+var p = document.createElement('p');
+var saida = document.getElementById('saida');
+saida.appendChild(p);
+ 
+var fm = document.forms[0]
+
+var el = fm.elements
+
+el[0].value = "Felipe Dantas";
